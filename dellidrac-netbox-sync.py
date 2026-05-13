0e791a7886abe80bb887b25537ee09c5bac3a042
Neelesh Sabat <neeleshsabat@Neeleshs-MacBook-Pro.local>
2026-05-13 11:27:54 +0530

    Created the sync.py file

---
+++ dellidrac-netbox-sync.py
@@ -0,0 +1,4 @@
+# Logic to sync Dell iDRAC devices with NetBox
+'''
+Here is the code to sync Dell iDRAC devices with NetBox. This script will connect to the iDRAC interface, retrieve the necessary information about the devices, and then update or create entries in NetBox accordingly.
+'''
